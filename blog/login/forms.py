3c4758de141289b9d0a284ce5af40a4b22bb1686
Sofia <wwww@corp.nstu.ru>
2026-04-08 11:02:22 +0700

    добавлена вьюшка для создания пользователя

---
+++ blog/login/forms.py
@@ -0,0 +1,14 @@
+from .models import User
+from django import forms
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['login', 'password', 'first_name', 'last_name', 'age']
+        labels = {
+            'login': 'Логин',
+            'password': 'Пароль',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'age': 'Возраст',
+        }
